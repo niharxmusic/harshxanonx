958aee37662c84f17e11f9506e0870e56b2b6782
niharxmusic <124661733+niharxmusic@users.noreply.github.com>
2023-02-07 20:58:07 +0530

    Delete __init__.py

--- harsh/__init__.py
+++
@@ -1,8 +0,0 @@
-FROM Pyrogram import client
-
-
-API_ID = int(25130753)
-API_HASH = "6186269129:AAGOibCRAvXgainSN7MkaStuFLOHENvhKj0"
-
-#Botting process
-app = client(name="OK", api_id=API_ID, api_hash=API_HASH, bot_token, in_memory=true, plugins=dict(root="nihar/plugins")       
