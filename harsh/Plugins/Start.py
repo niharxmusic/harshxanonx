406536c988a0805fa4d5b5533081122e37de469a
niharxmusic <124661733+niharxmusic@users.noreply.github.com>
2023-02-07 19:24:06 +0530

    Update Start.py

--- harsh/Plugins/Start.py
+++ harsh/Plugins/Start.py
@@ -2,7 +2,7 @@ Form Pyrogram import filters
 Form nihar import app
 
 
-@app.on_message(filters.command(["start",
+@app.on_message(filters.command(["start"  "st"]) & filters       
  
 async def _(aap, message): 
      await message.reply_text("Hii There") 
