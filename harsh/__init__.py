e62dcd8025788cda2c55703ec7edfdeffee56f69
niharxmusic <124661733+niharxmusic@users.noreply.github.com>
2023-02-07 18:56:25 +0530

    Create __init__.py

---
+++ harsh/__init__.py
@@ -0,0 +1,8 @@
+FROM Pyrogram import client
+
+
+API_ID = int(25130753)
+API_HASH = "a83e78bf91141cc1e33b746ca2b3ac13"
+
+#Botting process
+app = client(name="OK", api_id=API_ID, api_hash=API_HASH, bot_token, in_memory=true, plugins=dict(root="nihar/plugins")       
