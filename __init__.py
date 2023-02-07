ed0db2a72bc53df615a7899e197a5165cc1a5b9f
niharxmusic <124661733+niharxmusic@users.noreply.github.com>
2023-02-07 18:38:57 +0530

    Create __init__.py

---
+++ __init__.py
@@ -0,0 +1,8 @@
+Form Pyrogram import client 
+
+API_ID = int(25130753)
+API_HASH = "a83e78bf91141cc1e33b746ca2b3ac13"
+BOT_TOKEN = "6013020115:AAEAJiMxjtFjXDyCduWliAWAYT1Pm5ztpDA"
+
+#Botting process 
+app = client(name="okk", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN, in_memory=true, plugins=dict(root="nihar"/plugins      
