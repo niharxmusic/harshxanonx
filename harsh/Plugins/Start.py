a56f5264359e16ba710c1ca268bd04ae8fa01d45
niharxmusic <124661733+niharxmusic@users.noreply.github.com>
2023-02-07 20:58:19 +0530

    Delete Start.py

--- harsh/Plugins/Start.py
+++
@@ -1,6 +0,0 @@
-Form Pyrogram import filters, client 
-
-@client.on_message(filters.command(["start"  "st"]) & filters       
- 
-async def _(app: client, message): 
-     await message.reply_text("Hii There") 
